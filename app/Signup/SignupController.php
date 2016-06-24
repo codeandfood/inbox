78be78cb3c2ea996e85e7c5e2e438860985418a4
codeandfood <codeandfood@gmail.com>
2016-06-24 13:01:01 +0530

    first commit

---
+++ app/Signup/SignupController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Signup;
+
+use Illuminate\Http\Request;
+
+use App\Http\Requests;
+use App\Http\Controllers\Controller;
+
+class SignupController extends Controller
+{
+    function signup(){
+    	echo "Signup";
+    }
+}
